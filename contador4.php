8f5d709de47fbfb38b72672ea50da926dc187f06
a2107027 <122257170+a2107027@users.noreply.github.com>
2023-01-09 12:35:15 +0100

    contador4

--- contador4.php
+++ contador4.php
@@ -3,6 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+
 $servername = "localhost";
 $username = "admin";
 $password = "12345";
@@ -11,12 +12,13 @@ $dbname = "estadistics";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if($conn -> connect_errno) {
-    echo "Error en connectar a MySQL: " .$conn -> connect_error;
-    exit();
+   echo "Error en connectar a MySQL: " . $conn -> connect_error;
+   exit();
 }
 
 $ip=$_SERVER['REMOTE_ADDR'];
 $sql="INSERT INTO registre(ip) VALUES ('" . $ip . "')";
+$conn->query($sql);
 
 $resultat = $conn->query("SELECT COUNT(*) FROM registre");
 $row = mysqli_fetch_array($resultat);
